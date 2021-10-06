011d57a65b7daf860738b26b966b19e55f305372
pivo <84128815+heavenlysouffle@users.noreply.github.com>
2021-10-07 01:38:50 +0300

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -3,22 +3,20 @@ import math
 
 class Rational:
     def __init__(self, num=0, denom=1):
-        try:
-            if not denom:
-                raise Exception
-            self.__num = num / math.gcd(num, denom)
-            self.__denom = denom / math.gcd(num, denom)
-        except:
-            print("Error")
-            exit()
+        if not isinstance(num, int) or not isinstance(denom, int):
+            raise TypeError("numerator and denominator must be of integer type")
+        if not denom:
+            raise ZeroDivisionError
+        self.__num = num // math.gcd(num, denom)
+        self.__denom = denom // math.gcd(num, denom)
 
     def fraction(self):
-        print(int(self.__num), '/', int(self.__denom))
+        return f'{self.__num} / {self.__denom}'
 
     def result(self):
-        print("result: ", self.__num / self.__denom)
+        return self.__num / self.__denom
 
 
 object1 = Rational(2, 4)
-object1.fraction()
-object1.result()
+print(object1.fraction())
+print(object1.result)
