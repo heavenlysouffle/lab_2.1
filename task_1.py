353c865180bd07218bbf4a4f5e8201a316cd877d
pivo <84128815+heavenlysouffle@users.noreply.github.com>
2021-10-19 19:27:11 +0300

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,7 +1,11 @@
 class Rectangle:
-    def __init__(self):
-        self.length = 1.0
-        self.width = 1.0
+    def __init__(self, length=1.0, width=1.0):
+        if not isinstance(length, float) or not isinstance(width, float):
+            raise TypeError("length and width must be of float type")
+        if length < 0.0 or length > 20.00 or width < 0.0 or width > 20.0:
+            raise ValueError("length and width must be in range (0, 20)")
+        self.length = length
+        self.width = width
 
     def perimeter(self):
         return 2 * (self.length + self.width)
